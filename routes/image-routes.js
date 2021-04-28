2b81ce98b44408f2e344d6fdf1d964cec5a1365e
Ben Hopkins <bjhops17@gmail.com>
2021-04-28 12:59:52 -0700

    added protected post route

--- routes/image-routes.js
+++ routes/image-routes.js
@@ -1,22 +1,47 @@
-const router = require("express").Router()
-const db = require("../models")
-
-
+const router = require("express").Router();
+const db = require("../models");
 
 router.post("/api/singlepost", async (req, res) => {
-    const data = await db.Image.create(req.body).catch(err => res.status(404).json(err))
-    res.json(data)
-})
+  let token = false;
+  if (!req.headers) {
+    token = false;
+  } else if (!req.headers.authorization) {
+    token = false;
+  } else {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  if (!token) {
+    res.status(403).send("Please Login");
+  } else {
+    let tokenMatch = jwt.verify(token, process.env.JWS_TOKEN, (err, verify) => {
+      if (err) {
+        return false;
+      } else {
+        return verify;
+      }
+    });
+    if (tokenMatch) {
+      const data = await db.Image.create(req.body).catch((err) =>
+        res.status(404).json(err)
+      );
+      res.json(data);
+    } else {
+      res
+        .status(403)
+        .json(
+          "Authorization Error! You either do not have access or session has expired."
+        );
+    }
+  }
+});
 
 router.get("/api/getpublic", async (req, res) => {
-    const data = await db.Image.findAll({
-        where:{
-            public: true
-        }
-    }).catch(err => res.status(404).json(err))
-    res.json(data)
-})
+  const data = await db.Image.findAll({
+    where: {
+      public: true,
+    },
+  }).catch((err) => res.status(404).json(err));
+  res.json(data);
+});
 
-
-
-module.exports = router
+module.exports = router;
